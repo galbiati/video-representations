87d92807a961e7f45684026368eeaff1b76b94c1
gianni <galbiatig@gmail.com>
2017-07-27 11:36:51 -0400

    cleanup; begin sequence loader

--- load_data.py
+++ load_data.py
@@ -1,12 +1,15 @@
+import os
 import numpy as np
 
 def load_data(filepath):
+    """Loads data from a npz file into a list of videos as numpy arrays"""
     with np.load(filepath) as f:
         frames = [arr for name, arr in f.items()]
 
     return list(frames[0])
 
-def load_all_data(data_dir, skip_first=0, every_n=1):
+def load_all_data_stacked(data_dir, skip_first=0, every_n=1):
+    """Loads data from all npz files and stacks data into 4D numpy arrays"""
     filepaths = [os.path.join(data_dir, f) for f in os.listdir(data_dir)]
     frames = [load_data(filepath) for filepath in filepaths[skip_first::every_n]]
     X = np.concatenate([np.concatenate([f[:-1] for f in frame]) for frame in frames])
@@ -14,7 +17,17 @@ def load_all_data(data_dir, skip_first=0, every_n=1):
 
     return X, y
 
+def load_all_data_sequences(data_dir, skip_first=0, every_n=1):
+    filepaths = [os.path.join(data_dir, f) for f in os.listdir(data_dir)]
+    frames = [load_data(filepath) for filepath in filepaths[skip_first::every_n]]
+
+    return frames
+
 def get_splits(X, y, ratio=10):
+    """Get training and test splits from numpy arrays"""
+
+    assert X.shape[0] == y.shape[0]
+
     idxs = np.arange(X.shape[0])
     np.random.shuffle(idxs)
     train_idxs = idxs[(idxs.size//ratio):]
@@ -28,7 +41,9 @@ def get_splits(X, y, ratio=10):
     return X_train, y_train, X_test, y_test
 
 def pad_frames(frames, final_length=20):
-    pad = np.zeros(3, 60, 80), dtype=np.int8)[np.newaxis, :, :, :]
+    """Pad videos by prepending empty frames to have same length"""
+
+    pad = np.zeros((3, 60, 80), dtype=np.int8)[np.newaxis, :, :, :]
     if frames.shape[0] <= 20:
         padstack = np.repeat(pad, final_length - frames.shape[0], axis=0)
         return np.concatenate((padstack, frames))
